a3789009017a90d7752ef27be900a7dadc008c68
adnanelhabashy <adnan.elhabashy@live.com>
2025-03-18 13:24:50 +0200

    added tail wind css

---
+++ egx-web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-red-500 p-4 text-white">
+      This should have a red background
+    </div>
+  );
+}
+
+export default App;
